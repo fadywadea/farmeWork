764151e37d09782a942619b8b83aaa2026c0ddfa
fadywadea <fwadea3@gmail.com>
2023-09-22 11:26:29 +0300

    commit

--- src/Components/Contacts/Contacts.jsx
+++ src/Components/Contacts/Contacts.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable eqeqeq */
 import React from 'react'
 import { useState } from 'react'
-import Style from './Contacts.module.css'
 
 export default function Contacts() {
 
@@ -25,28 +24,28 @@ export default function Contacts() {
 
   return <>
     <div className='mb-4'>
-      <div className={`${Style.contact} pt-4`}>
+      <div className='contact pt-4'>
         <div className='text-center pt-4' >
           <h2 className='text-uppercase mb-3 fs-1 fw-bolder'>contact section</h2>
           <div className='d-flex align-items-center justify-content-center mb-3'>
-            <div className={`${Style.lines}  me-3`}></div>
+            <div className='lines  me-3'></div>
             <div className='fa-solid fa-star star'></div>
-            <div className={`${Style.lines}  ms-3`}></div>
+            <div className='lines  ms-3'></div>
           </div>
         </div>
 
         <form className='w-50 p-3 mx-auto mt-5 ng-pristine ng-valid ng-touched'>
 
-          <label htmlFor="userName" className={labelActive ? `${Style.colorLabel} position-relative top-0` : `${Style.__top} position-relative top-0`}>userName :</label>
+          <label htmlFor="userName" className={labelActive ? 'colorLabel position-relative top-0' : '__top position-relative top-0'}>userName :</label>
           <input className='form-control border-0 border-bottom py-3 position-relative' placeholder='userName' name='userName' id='userName' type="text" />
 
-          <label htmlFor="userAge" className={labelActive ? `${Style.colorLabel} position-relative top-0` : `${Style.__top} position-relative top-0`} >userAge :</label>
+          <label htmlFor="userAge" className={labelActive ? 'colorLabel position-relative top-0' : '__top position-relative top-0'} >userAge :</label>
           <input className='form-control border-0 border-bottom py-3 position-relative' placeholder='userAge' name='userAge' id='userAge' type="text" />
 
-          <label htmlFor="userEmail" className={labelActive ? `${Style.colorLabel} position-relative top-0` : `${Style.__top} position-relative top-0`} >userEmail :</label>
+          <label htmlFor="userEmail" className={labelActive ? 'colorLabel position-relative top-0' : '__top position-relative top-0'} >userEmail :</label>
           <input className='form-control border-0 border-bottom py-3 position-relative' placeholder='userEmail' name='userEmail' id='userEmail' type="text" />
 
-          <label htmlFor="userPassword" className={labelActive ? `${Style.colorLabel} position-relative top-0` : `${Style.__top} position-relative top-0`} >userPassword :</label>
+          <label htmlFor="userPassword" className={labelActive ? 'colorLabel position-relative top-0' : '__top position-relative top-0'} >userPassword :</label>
           <input className='form-control border-0 border-bottom py-3 position-relative' placeholder='userPassword' name='userPassword' id='userPassword' type="text" />
 
           <button className="btn mt-4 text-white"> send Message </button>
